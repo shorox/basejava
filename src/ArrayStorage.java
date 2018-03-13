30de4b330857dd0db9cdba0c5df2672a9db0bee8
gagarina6794 <gagarina6794@gmail.com>
2018-03-13 14:03:30 +0200

    realised methods in class according to proposes

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,60 +1,46 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-
     Resume[] storage = new Resume[10000];
-    private int size = 0;
+    private int size;
 
     void clear() {
-
-      storage = new Resume[10000];
-      size = 0;
-
+        Arrays.fill(storage, null);
+        size = 0;
     }
 
     void save(Resume r) {
-
         storage[size] = r;
         size++;
-
     }
 
     Resume get(String uuid) {
-
         for (int i = 0; i < size; i++) {
-
-            if (storage[i].toString().equals(uuid)) {
+            if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
             }
         }
-
         return null;
     }
 
     void delete(String uuid) {
-
         for (int i = 0; i < size; i++) {
-
-            if (storage[i].toString().equals(uuid)) {
+            if (storage[i].uuid.equals(uuid)) {
                 size--;
                 storage[i] = storage[size];
                 storage[size] = null;
             }
         }
-
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-
-        Resume[] storageNew = new Resume[size];
-
-        System.arraycopy(storage, 0, storageNew, 0, size);
-
-        return storageNew;
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
