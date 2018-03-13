3a363b40f2762e1945d25150baf7a1b58ac7c61f
gagarina6794 <gagarina6794@gmail.com>
2018-03-13 12:56:21 +0200

    realise methods in class

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,29 +2,62 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
+
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
+
+      storage = new Resume[10000];
+      size = 0;
+
     }
 
     void save(Resume r) {
+
+        storage[size] = r;
+        size++;
+
     }
 
     Resume get(String uuid) {
+
+        for (int i = 0; i < size; i++) {
+
+            if (storage[i].toString().equals(uuid)) {
+                return storage[i];
+            }
+        }
+
         return null;
     }
 
     void delete(String uuid) {
+
+        for (int i = 0; i < size; i++) {
+
+            if (storage[i].toString().equals(uuid)) {
+                size--;
+                storage[i] = storage[size];
+                storage[size] = null;
+            }
+        }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+
+        Resume[] storageNew = new Resume[size];
+
+        System.arraycopy(storage, 0, storageNew, 0, size);
+
+        return storageNew;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
