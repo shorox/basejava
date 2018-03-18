4bc3845e5ed284361a3093d995b0edb94365cc2e
gagarina6794 <gagarina6794@gmail.com>
2018-03-18 06:54:55 +0200

    add method update & isPresent

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,20 +4,36 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
     private int size;
 
-    void clear() {
-        Arrays.fill(storage, 0,size-1,null);
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    void save(Resume r) {
-        storage[size] = r;
-        size++;
+    public void update(Resume r) {
+        for (int i = 0; i < size; i++) {
+                if (storage[i].uuid.equals(r.uuid)) {
+                    storage[i] = r;
+                    return;
+                }
+            }
+        System.out.println("ERROR: Resume was not found for update!");
     }
 
-    Resume get(String uuid) {
+    public void save(Resume r) {
+        if (!isPresent(r)) {
+            try {
+                storage[size] = r;
+                size++;
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("ERROR: Free space is out for this save!");
+            }
+        } else System.out.println("ERROR: Resume is already exist!");
+    }
+
+    public Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
@@ -26,24 +42,35 @@ public class ArrayStorage {
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 size--;
                 storage[i] = storage[size];
                 storage[size] = null;
+                return;
             }
         }
+        System.out.println("ERROR: Resume was not found for delete!");
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
 
-    int size() {
+    public int size() {
         return size;
+    }
+
+    private boolean isPresent(Resume r) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(r.uuid)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
