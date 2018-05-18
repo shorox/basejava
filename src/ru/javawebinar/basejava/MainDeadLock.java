f2837a051cbddae6e616a1223e819c268d2b491f
gagarina6794 <gagarina6794@gmail.com>
2018-05-18 09:21:47 +0300

    HW 11 DeadLock one method

--- src/ru/javawebinar/basejava/MainDeadLock.java
+++ src/ru/javawebinar/basejava/MainDeadLock.java
@@ -4,35 +4,33 @@ public class MainDeadLock {
 
     public final static Object firstObject = new Object();
     public final static Object secondObject = new Object();
+    private static java.lang.Object Object;
 
     public static void main(String[] args) {
 
-        Thread t1 = new Thread(() -> {
-            System.out.println("Lock first Object in t1");
-            synchronized (firstObject) {
-                System.out.println("Go to sleep in t1");
-                Thread.yield();
-                System.out.println("We sleeping in t1");
-                System.out.println("Lock second Object in t1");
-                synchronized (secondObject) {
-                    System.out.println("Lock t1!");
-                }
-            }
-        });
-        Thread t2 = new Thread(() -> {
-            System.out.println("Lock second Object in t2");
-            synchronized (secondObject) {
-                System.out.println("Go to sleep in t2");
-                Thread.yield();
-                System.out.println("We sleeping in t2");
-                System.out.println("Lock first Object in t2");
-                synchronized (firstObject) {
-                    System.out.println("Lock t2!");
-                }
-            }
-        });
+        Thread t1 = createThread(firstObject, secondObject);
+        Thread t2 = createThread(secondObject, firstObject);
 
         t1.start();
         t2.start();
+    }
+
+    public static Thread createThread(Object o1, Object o2) {
+        return new Thread(() -> {
+            System.out.println("Lock first Object in " + Thread.currentThread().getName());
+            synchronized (o1) {
+                System.out.println("Go to sleep in " + Thread.currentThread().getName());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("We sleeping in " + Thread.currentThread().getName());
+                System.out.println("Lock second Object in " + Thread.currentThread().getName());
+                synchronized (o2) {
+                    System.out.println("Lock  " + Thread.currentThread().getName());
+                }
+            }
+        });
     }
 }
