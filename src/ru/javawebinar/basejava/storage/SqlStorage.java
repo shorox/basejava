b7609760b3fbb8d3607c483fb3997e52f2d4bbfd
gagarina6794 <1331gagarina>
2018-10-29 14:42:10 +0200

    edit SQLStorage

--- src/ru/javawebinar/basejava/storage/SqlStorage.java
+++ src/ru/javawebinar/basejava/storage/SqlStorage.java
@@ -127,20 +127,6 @@ public class SqlStorage implements Storage {
                 }
             }
         }
-//        sqlHelper.execute("DELETE FROM contact WHERE resume_uuid=?", ps -> {
-//            ps.setString(1, uuid);
-//            if (ps.executeUpdate() == 0) {
-//                throw new NotExistStorageException(uuid);
-//            }
-//            return null;
-//        });
-//        sqlHelper.execute("DELETE FROM category WHERE resume_uuid=?", ps -> {
-//            ps.setString(1, uuid);
-//            if (ps.executeUpdate() == 0) {
-//                throw new NotExistStorageException(uuid);
-//            }
-//            return null;
-//        });
         sqlHelper.execute("DELETE FROM resume WHERE uuid=?", ps -> {
             ps.setString(1, uuid);
             if (ps.executeUpdate() == 0) {
